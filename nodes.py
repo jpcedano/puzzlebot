a990072e3b89c4c9cf36dd0ce0df5eecd6c56130
José Pablo Cedano <83967893+jpcedano@users.noreply.github.com>
2024-05-31 10:45:49 -0600

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -32,23 +32,10 @@ class LineFollower(Node):
     def timer_callback(self):
         if self.image_received_flag:
             image = self.cv_img.copy()
-            #height, width = image.shape[:2]
-            #top_left = (width // 6,height // 2)
-            #bottom_right = (5* width // 6, height)
-            #blank = np.zeros(image.shape[:2], dtype=np.uint8)
-            #mask = cv2.rectangle(blank,top_left,bottom_right,255,thickness=cv2.FILLED)
-            #masked_image= cv2.bitwise_and(image,image,mask=mask)
 
             region_of_interest = image[320:360,70:200]
-            #blurred = cv2.GaussianBlur(image, (5, 5), 0) # gaussian filter
             gray = cv2.cvtColor(region_of_interest, cv2.COLOR_BGR2GRAY)
             _, thresholded = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)
-            #cv2.imshow("Seguidor de Linea",thresholded)
-            #if cv2.waitKey(1) & 0xFF == ord('q'):
-                #self.get_logger().info("Exit requested by user.")
-                #rclpy.shutdown()
-            # Adaptative threshold
-            #thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2) 
         
             contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             if contours:
@@ -59,7 +46,7 @@ class LineFollower(Node):
                     cx = int(M['m10']/M['m00'])
                     cy = int(M['m01']/M['m00'])
                     # Draw a circle at the center of the line
-                    mark_size = 2  # Puedes ajustar esto según la estética deseada
+                    mark_size = 2  
 
                     # Dibujar un círculo en el centro de la línea
                     cv2.circle(region_of_interest, (cx, cy), mark_size, (0, 255, 0), -1)
@@ -70,20 +57,13 @@ class LineFollower(Node):
                     print('Centro de imagen:', img_center)
                     print('Posicion de linea:', cx)
                     print('Error:', error)
-
+                    
+                    self.error.data = error
                     self.error_pub.publish(self.error)
 
-                    #self.robot_vel.angular.z = -float(error) / 200  # P-controller for steering
-                    #self.robot_vel.linear.x = 0.15  # Constant forward speed
-                #else:
-                    #self.robot_vel.angular.z = 0.0
-                    #self.robot_vel.linear.x = 0.0
             else:
                 self.get_logger().info('No line detected')
-                #self.robot_vel.angular.z = 0.0
-                #self.robot_vel.linear.x = 0.0
 
-            #self.cmd_vel_pub.publish(self.robot_vel)
             self.pub.publish(self.bridge.cv2_to_imgmsg(region_of_interest, 'bgr8'))
 
 def main(args=None):
