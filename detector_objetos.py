ae86c761645a72e9a59fea2f38c19290e3c1403e
José Pablo Cedano <83967893+jpcedano@users.noreply.github.com>
2024-06-05 19:42:50 +0100

    Update detector_objetos.py

--- detector_objetos.py
+++ detector_objetos.py
@@ -9,7 +9,7 @@ from ultralytics import YOLO
 class YoloV8Node(Node):
     def __init__(self):
         super().__init__('yolo_v8_node')
-        self.publisher_image = self.create_publisher(Image, 'annotated_frames', 10)
+        #self.publisher_image = self.create_publisher(Image, 'annotated_frames', 10)
         self.publisher_label = self.create_publisher(String, 'detected_labels', 10)
         self.subscription = self.create_subscription(Image, 'video_source/raw', self.image_callback, 10)
         self.bridge = CvBridge()
@@ -26,38 +26,21 @@ class YoloV8Node(Node):
         # Perform inference on the frame
         results = self.model(frame)
 
-        # Check if results are empty
-        if not results:
-            self.get_logger().info('No detection results.')
-            return
-        
-        self.get_logger().info('Inference completed.')
-
         # Annotate the frame with detection results
-        annotated_frame = results[0].plot()
+        #annotated_frame = results[0].plot()
 
         # Convert OpenCV image to ROS 2 Image Message
-        img_msg = self.bridge.cv2_to_imgmsg(annotated_frame, encoding='bgr8')
+        #img_msg = self.bridge.cv2_to_imgmsg(annotated_frame, encoding='bgr8')
 
         # Publish the annotated frame
-        self.publisher_image.publish(img_msg)
-        self.get_logger().info('Annotated frame published.')
-
+        #self.publisher_image.publish(img_msg)
+        
         # Extract and publish labels
-        if hasattr(results[0], 'boxes') and hasattr(results[0].boxes, 'cls'):
-            labels = []
-            for cls in results[0].boxes.cls:
-                label_index = int(cls)
-                if label_index in self.model.names:
-                    labels.append(self.model.names[label_index])
-                else:
-                    self.get_logger().warn(f'Label index {label_index} not found in model names.')
-            labels_str = ', '.join(labels)
-            self.get_logger().info(f'Detected labels: {labels_str}')
-            self.publisher_label.publish(String(data=labels_str))
-        else:
-            self.get_logger().info('No labels to publish.')
-
+        labels = [self.model.names[int(cls)] for cls in results[0].boxes.cls]
+        labels_str = ', '.join(labels)
+        print("Published label", labels_str)
+        self.publisher_label.publish(String(data=labels_str))
+        
 def main(args=None):
     rclpy.init(args=args)
     node = YoloV8Node()
@@ -70,4 +53,4 @@ def main(args=None):
         rclpy.shutdown()
 
 if __name__ == '__main__':
-    main()
+    main() 
