fc0d987bcd392f5747108f8ca33f5009ef55115c
José Pablo Cedano <83967893+jpcedano@users.noreply.github.com>
2024-06-11 00:02:02 +0100

    Update velocidad.py

--- velocidad.py
+++ velocidad.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32, Bool, Float32, String
+from std_msgs.msg import Int32, Bool, String, Float32
 from geometry_msgs.msg import Twist
 from rclpy.qos import QoSProfile, ReliabilityPolicy
 import time
@@ -12,19 +12,22 @@ class LineFollower(Node):
         self.error_sub = self.create_subscription(Int32, 'error', self.error_callback, 10)
         self.contour_sub = self.create_subscription(Bool, 'FindContour', self.contour_callback, 10)
         self.objetos_sub = self.create_subscription(String, 'detected_labels', self.objetos_callback, 10)
-        self.semaforo_sub = self.create_subscription(Float32,'signal_value',self.semaforo_callback,10)
+        self.semaforo_sub = self.create_subscription(String, 'signal_value', self.semaforo_callback, 10)
+        self.angle_publisher = self.create_subscription(Float32, 'odom_angle', self.angulo_callback,10)
+
 
         qos_profile = QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT)
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', qos_profile)
         
         self.robot_vel = Twist()
         self.error = 0
-        self.semaforo = ""
         self.semaforo_detectado = ""
         self.contour = False
         self.objetos = ""
         self.objeto_detectado = ""
-        signal_value = Float32()
+        self.semaforo_value = 1.0
+        self.angulo = 0.0
+
 
         self.turnleft_signal_detected = False  # Flag for turn left signal detection
         self.round_signal_detected = False
@@ -46,41 +49,41 @@ class LineFollower(Node):
         self.objeto_detectado = self.objetos[0]
 
     def semaforo_callback(self, msg):
-        self.semaforo = msg.data
-        self.semaforo_detectado = self.semaforo
+        self.semaforo_detectado = msg.data
         print("Semaforo callback - color:", self.semaforo_detectado)
 
-    def timer_callback(self):
+    def angulo_callback(self, msg):
+        self.angulo = msg.data
 
-        signal_value = 1.0
-        
-        if self.semaforo_detectado == "red_light":
-            signal_value = 0.0
-        elif self.semaforo_detectado == "yellow_light":
-            signal_value = 0.5
-        else:
-            pass
+    def timer_callback(self):
 
         if self.contour:
             if self.objeto_detectado == 'workers_sgl':
                 self.robot_vel.angular.z = (-float(self.error) / 400)  # P-controller for steering
                 self.robot_vel.linear.x = 0.05 # Adjusted forward speed   
-                self.cmd_vel_pub.publish(self.robot_vel)   
-                                
+                self.cmd_vel_pub.publish(self.robot_vel)                         
+                
             else:
-                self.robot_vel.angular.z = (-float(self.error) / 400)  # P-controller for steering
-                self.robot_vel.linear.x = 0.15 * signal_value  # Adjusted forward speed
+                if self.semaforo_detectado == "red_light":
+                    self.signal_value = 0.0
+                elif self.semaforo_detectado == "yellow_light":
+                    self.signal_value = 0.5
+                else:
+                    self.signal_value = 1.0
+
+                self.robot_vel.angular.z = (-float(self.error) / 400)*self.signal_value  # P-controller for steering
+                self.robot_vel.linear.x = 0.15 * self.signal_value  # Adjusted forward speed
                 self.cmd_vel_pub.publish(self.robot_vel)
 
-                print("color: ", self.semaforo_detectado)
-                print("valor: ", signal_value)
-
+    
         elif self.objeto_detectado:
             if not self.contour and self.objeto_detectado == 'turnleft_sgl':
                 if not self.turnleft_signal_detected:
                     self.turnleft_signal_detected = True  # Set flag to True
+                    start_time = time.time()
+                    duration = 0.4
 
-                    for _ in range(300):
+                    for _ in range(200):
                         self.robot_vel.angular.z = -0.01
                         self.robot_vel.linear.x = 0.0
                         self.cmd_vel_pub.publish(self.robot_vel)
@@ -89,11 +92,13 @@ class LineFollower(Node):
                         self.robot_vel.angular.z = 0.0
                         self.robot_vel.linear.x = 0.1
                         self.cmd_vel_pub.publish(self.robot_vel)
-                    
-                    for _ in range(1350):
+
+                    while self.angulo <= 75.0:
                         self.robot_vel.angular.z = 0.05
                         self.robot_vel.linear.x = 0.0
-                        self.cmd_vel_pub.publish(self.robot_vel)                    
+                        self.cmd_vel_pub.publish(self.robot_vel)
+                        print("valor: 1 ", self.angulo)
+                    print("valor: 2 ", self.angulo)
 
                 
                     if self.objeto_detectado != 'turnleft_sgl':
@@ -102,12 +107,12 @@ class LineFollower(Node):
             elif not self.contour and self.objeto_detectado == 'round_sgl':
                 if not self.round_signal_detected:
                     self.round_signal_detected = True  # Set flag to True
-                    for _ in range(500):
+                    for _ in range(200):
                         self.robot_vel.angular.z = 0.0
                         self.robot_vel.linear.x = 0.1
                         self.cmd_vel_pub.publish(self.robot_vel)
 
-                    for _ in range(1500):
+                    for _ in range(1300):
                         self.robot_vel.angular.z = -0.05
                         self.robot_vel.linear.x = 0.0
                         self.cmd_vel_pub.publish(self.robot_vel)
@@ -119,7 +124,7 @@ class LineFollower(Node):
                 if not self.straight_signal_detected:
                     self.straight_signal_detected = True
 
-                    for _ in range(300):
+                    for _ in range(200):
                         self.robot_vel.angular.z = -0.05
                         self.robot_vel.linear.x = 0.0
                         self.cmd_vel_pub.publish(self.robot_vel)
@@ -146,10 +151,10 @@ class LineFollower(Node):
                         self.robot_vel.linear.x = 0.0
                         self.cmd_vel_pub.publish(self.robot_vel)
 
-                    for _ in range(200):
-                        self.robot_vel.angular.z = -0.05
-                        self.robot_vel.linear.x = 0.0
-                        self.cmd_vel_pub.publish(self.robot_vel)                
+                    #for _ in range(200):
+                        #self.robot_vel.angular.z = -0.05
+                        #self.robot_vel.linear.x = 0.0
+                        #self.cmd_vel_pub.publish(self.robot_vel)                
 
                     for _ in range(1000):
                         self.robot_vel.angular.z = 0.0
